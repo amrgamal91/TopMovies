15a390fd897d0888072a2df0bac3fb91f4e3eb8e
gemy91 <ag.amrgamal91@gmail.com>
2019-04-02 23:00:49 +0300

    Fix : Crash before render (BrowserRouter)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
 import Movies from "./components/movies";
-import { Route, Redirect, Switch } from "react-router-dom";
+import { Route, Redirect, Switch, BrowserRouter } from "react-router-dom";
 import MovieForm from "./components/movieForm";
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Switch>
-          <Route path="/movies/:id" component={MovieForm} />
-          <Route path="/movies" component={Movies} />
-          <Redirect from="/" exact to="/movies" />
-        </Switch>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Redirect from="/" exact to="/movies" />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
