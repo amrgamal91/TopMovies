63f1586560a5d6fa7372a1e01808d5f9f60961c8
gemy91 <ag.amrgamal91@gmail.com>
2019-11-27 13:17:27 +0300

    Updates

---
+++ precache-manifest.3317ff6b5eb22c28b73e4147ed591d74.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "1acabb0b9639b4fec0ae",
+    "url": "/TopMovies/static/css/main.c7c522a5.chunk.css"
+  },
+  {
+    "revision": "1acabb0b9639b4fec0ae",
+    "url": "/TopMovies/static/js/main.1acabb0b.chunk.js"
+  },
+  {
+    "revision": "583098a2299bc7667642",
+    "url": "/TopMovies/static/js/runtime~main.583098a2.js"
+  },
+  {
+    "revision": "008b685cc9d9917b53f0",
+    "url": "/TopMovies/static/css/2.3c06308c.chunk.css"
+  },
+  {
+    "revision": "008b685cc9d9917b53f0",
+    "url": "/TopMovies/static/js/2.008b685c.chunk.js"
+  },
+  {
+    "revision": "54077faacb8c4ec67a41ccc56c62fb00",
+    "url": "/TopMovies/static/media/cinema-28.54077faa.jpg"
+  },
+  {
+    "revision": "ff6e5c7fc7a212f15e474be1af7dec78",
+    "url": "/TopMovies/static/media/spinner-loading.ff6e5c7f.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/TopMovies/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/TopMovies/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/TopMovies/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/TopMovies/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/TopMovies/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "80ffeeeacbd972226f10089a962d0bc4",
+    "url": "/TopMovies/index.html"
+  }
+];
